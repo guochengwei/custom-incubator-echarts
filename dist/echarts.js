7825b2b331ef94051dcf9695e929b9e7071288f5
guochengwei <guochengwei@360.cn>
2019-03-07 19:20:19 +0800

    treeCollapse

--- dist/echarts.js
+++ dist/echarts.js
@@ -49177,186 +49177,192 @@ function getEdgeShape(seriesScope, sourceLayout, targetLayout) {
 * specific language governing permissions and limitations
 * under the License.
 */
-
 /**
  * @file Register the actions of the tree
  * @author Deqing Li(annong035@gmail.com)
  */
-
 registerAction({
-    type: 'treeExpandAndCollapse',
-    event: 'treeExpandAndCollapse',
-    update: 'update'
+  type: 'treeExpandAndCollapse',
+  event: 'treeExpandAndCollapse',
+  update: 'update'
 }, function (payload, ecModel, api) {
-    ecModel.eachComponent({mainType: 'series', subType: 'tree', query: payload}, function (seriesModel) {
-        var dataIndex = payload.dataIndex;
-        var dataName = payload.dataName;
-        var dataKey = payload.dataKey;
-        var data = seriesModel.getData();
-        var tree = data.tree;
-        var node = null;
-        if (dataIndex) {
-            node = tree.getNodeByDataIndex(dataIndex);
-        }
-        else if (dataName) {
-            node = tree.getNodeListByName(dataName);
-            node = node.length === 1
-                   ? node[0]
-                   : node.find(function (item) {
-                    return data.getRawDataItem(item.dataIndex).key === dataKey;
-                });
-        }
-        else if (dataKey) {
+  ecModel.eachComponent({ mainType: 'series', subType: 'tree', query: payload }, function (seriesModel) {
+    var dataIndex = payload.dataIndex;
+    var dataName = payload.dataName;
+    var dataKey = payload.dataKey;
+    var data = seriesModel.getData();
+    var tree = data.tree;
+    var node = null;
+    if (dataIndex) {
+      node = tree.getNodeByDataIndex(dataIndex);
+    }
+    else if (dataName) {
+      node = tree.getNodeListByName(dataName);
+      node = node.length === 1
+             ? node[0]
+             : node.find(function (item) {
+          return data.getRawDataItem(item.dataIndex).key === dataKey;
+        });
+    }
+    else if (dataKey) {
+      try {
+        data.each(function (idx) {
+          if (data.getRawDataItem(idx).key === dataKey) {
+            node = tree.getNodeByDataIndex(idx);
+            throw new Error('break');
+          }
+        });
+      }
+      catch (e) { }
+    }
+    if (!node) {
+      payload = null;
+      return;
+    }
+    if (node.isExpand && node.isActive) {
+      node.isExpand = false;
+    }
+    else {
+      node.isExpand = true;
+    }
+    if (payload.expand !== undefined) {
+      node.isExpand = payload.expand;
+    }
+    payload.dataIndex = node.dataIndex;
+    payload.child = node.children.length > 0;
+    payload.depth = node.depth;
+    tree.root.eachNode(function (item) {
+      item.isActive = false;
+      var el = data.getItemGraphicEl(item.dataIndex);
+      el && el.downplay();
+      el && el.__edge && el.__edge.trigger('normal');
+    });
+    node.getAncestors(true).forEach(function (item) {
+      if (!item.isActive) {
+        item.isActive = true;
+        var el = data.getItemGraphicEl(item.dataIndex);
+        el && el.highlight();
+        el && el.__edge && el.__edge.trigger('emphasis');
+      }
+    });
+  });
+});
+registerAction({
+  type: 'treeCollapse',
+  event: 'treeCollapse',
+  update: 'update'
+}, function (payload, ecModel) {
+  ecModel.eachComponent({ mainType: 'series', subType: 'tree', query: payload }, function (seriesModel) {
+    var data = seriesModel.getData();
+    var tree = data.tree;
+    tree.root.eachNode(function (item) {
+      item.isActive = false;
+      item.isExpand = false;
+    });
+  });
+});
+registerAction({
+  type: 'treeDownplay',
+  event: 'treeDownplay',
+  update: 'update'
+}, function (payload, ecModel) {
+  ecModel.eachComponent({ mainType: 'series', subType: 'tree', query: payload }, function (seriesModel) {
+    var data = seriesModel.getData();
+    var tree = data.tree;
+    tree.root.eachNode(function (item) {
+      item.isActive = false;
+      var el = data.getItemGraphicEl(item.dataIndex);
+      el && el.downplay();
+      el && el.__edge && el.__edge.trigger('normal');
+    });
+  });
+});
+registerAction({
+  type: 'treeSearchHighlight',
+  event: 'treeSearchHighlight',
+  update: 'update'
+}, function (payload, ecModel) {
+  ecModel.eachComponent({ mainType: 'series', subType: 'tree', query: payload }, function (seriesModel) {
+    var dataName = payload.dataName;
+    var data = seriesModel.getData();
+    var tree = data.tree;
+    var dataKey = payload.dataKey;
+    var nodeList = [];
+    if (dataName) {
+      nodeList = tree.getNodeListByName(dataName);
+      if (dataKey) {
+        nodeList = nodeList.filter(function (item) {
+          return item.key === dataKey;
+        });
+      }
+    }
+    else if (dataKey) {
+      if (Array.isArray(dataKey)) {
+        try {
+          data.each(function (idx) {
+            if (nodeList.length === dataKey.length) {
+              throw new Error('break');
+            }
+            var key = data.getRawDataItem(idx).key;
             try {
-                data.each(function (idx) {
-                    if (data.getRawDataItem(idx).key === dataKey) {
-                        node = tree.getNodeByDataIndex(idx);
-                        throw new Error('break');
-                    }
-                });
+              dataKey.forEach(function (item) {
+                if (key === item) {
+                  nodeList.push(tree.getNodeByDataIndex(idx));
+                  throw new Error('break');
+                }
+              });
             }
-            catch (e) { }
+            catch (e) {}
+          });
         }
-
-        if (!node) {
-            payload = null;
-            return;
-        }
-        if (node.isExpand && node.isActive) {
-            node.isExpand = false;
-        }
-        else {
-            node.isExpand = true;
-        }
-        if (payload.expand !== undefined) {
-            node.isExpand = payload.expand;
-        }
-        payload.dataIndex = node.dataIndex;
-        payload.child = node.children.length > 0;
-        payload.depth = node.depth;
-        tree.root.eachNode(function (item) {
-            item.isActive = false;
-            var el = data.getItemGraphicEl(item.dataIndex);
-            el && el.downplay();
-            el && el.__edge && el.__edge.trigger('normal');
-        });
-        node.getAncestors(true).forEach(function (item) {
-            if (!item.isActive) {
-                item.isActive = true;
-                var el = data.getItemGraphicEl(item.dataIndex);
-                el && el.highlight();
-                el && el.__edge && el.__edge.trigger('emphasis');
+        catch (e) { }
+      }
+      else {
+        try {
+          data.each(function (idx) {
+            if (data.getRawDataItem(idx).key === dataKey) {
+              nodeList.push(tree.getNodeByDataIndex(idx));
+              throw new Error('break');
             }
-        });
+          });
+        }
+        catch (e) { }
+      }
+    }
+    nodeList.forEach(function (node) {
+      node.getAncestors(true).forEach(function (item) {
+        if (!item.isActive) {
+          item.isActive = true;
+          var el = data.getItemGraphicEl(item.dataIndex);
+          el && el.highlight();
+          el && el.__edge && el.__edge.trigger('emphasis');
+        }
+        if (!item.isExpand) {
+          item.isExpand = true;
+        }
+      });
+      node.isActive = true;
     });
-});
-
-registerAction({
-    type: 'treeDownplay',
-    event: 'treeDownplay',
-    update: 'update'
-}, function (payload, ecModel) {
-    ecModel.eachComponent({mainType: 'series', subType: 'tree', query: payload}, function (seriesModel) {
-        var data = seriesModel.getData();
-        var tree = data.tree;
-        tree.root.eachNode(function (item) {
-            item.isActive = false;
-            var el = data.getItemGraphicEl(item.dataIndex);
-            el && el.downplay();
-            el && el.__edge && el.__edge.trigger('normal');
-        });
-    });
-});
-
-registerAction({
-    type: 'treeSearchHighlight',
-    event: 'treeSearchHighlight',
-    update: 'update'
-}, function (payload, ecModel) {
-    ecModel.eachComponent({mainType: 'series', subType: 'tree', query: payload}, function (seriesModel) {
-        var dataName = payload.dataName;
-        var data = seriesModel.getData();
-        var tree = data.tree;
-        var dataKey = payload.dataKey;
-        var nodeList = [];
-
-        if (dataName) {
-            nodeList = tree.getNodeListByName(dataName);
-            if (dataKey) {
-                nodeList = nodeList.filter(function (item) {
-                    return item.key === dataKey;
-                });
-            }
-        }
-        else if (dataKey) {
-            if (Array.isArray(dataKey)) {
-                try {
-                    data.each(function (idx) {
-                        if (nodeList.length === dataKey.length) {
-                            throw new Error('break');
-                        }
-                        var key = data.getRawDataItem(idx).key;
-                        try {
-                            dataKey.forEach(function (item) {
-                                if (key === item) {
-                                    nodeList.push(tree.getNodeByDataIndex(idx));
-                                    throw new Error('break');
-                                }
-                            });
-                        }
-                        catch (e) {}
-                    });
-                }
-                catch (e) { }
-            }
-            else {
-                try {
-                    data.each(function (idx) {
-                        if (data.getRawDataItem(idx).key === dataKey) {
-                            nodeList.push(tree.getNodeByDataIndex(idx));
-                            throw new Error('break');
-                        }
-                    });
-                }
-                catch (e) { }
-            }
-        }
-        nodeList.forEach(function (node) {
-            node.getAncestors(true).forEach(function (item) {
-                if (!item.isActive) {
-                    item.isActive = true;
-                    var el = data.getItemGraphicEl(item.dataIndex);
-                    el && el.highlight();
-                    el && el.__edge && el.__edge.trigger('emphasis');
-                }
-                if (!item.isExpand) {
-                    item.isExpand = true;
-                }
-            });
-            node.isActive = true;
-        });
-    });
+  });
 });
 registerAction({
-    type: 'treeRoam',
-    event: 'treeRoam',
-    // Here we set 'none' instead of 'update', because roam action
-    // just need to update the transform matrix without having to recalculate
-    // the layout. So don't need to go through the whole update process, such
-    // as 'dataPrcocess', 'coordSystemUpdate', 'layout' and so on.
-    update: 'none'
+  type: 'treeRoam',
+  event: 'treeRoam',
+  // Here we set 'none' instead of 'update', because roam action
+  // just need to update the transform matrix without having to recalculate
+  // the layout. So don't need to go through the whole update process, such
+  // as 'dataPrcocess', 'coordSystemUpdate', 'layout' and so on.
+  update: 'none'
 }, function (payload, ecModel) {
-    ecModel.eachComponent({mainType: 'series', subType: 'tree', query: payload}, function (seriesModel) {
-        var coordSys = seriesModel.coordinateSystem;
-        var res = updateCenterAndZoom(coordSys, payload);
-
-        seriesModel.setCenter
-        && seriesModel.setCenter(res.center);
-
-        seriesModel.setZoom
-        && seriesModel.setZoom(res.zoom);
-        payload.realZoom = res.zoom;
-    });
+  ecModel.eachComponent({ mainType: 'series', subType: 'tree', query: payload }, function (seriesModel) {
+    var coordSys = seriesModel.coordinateSystem;
+    var res = updateCenterAndZoom(coordSys, payload);
+    seriesModel.setCenter
+    && seriesModel.setCenter(res.center);
+    seriesModel.setZoom
+    && seriesModel.setZoom(res.zoom);
+    payload.realZoom = res.zoom;
+  });
 });
 
 /*
